4b3ba0edba6ee67992a69ab98dcd96115215288d
GorbachL <lenagorbach@coherentsolutions.com>
2020-06-04 22:04:41 +0300

    Added test to dev branch

--- src/test/java/tests/SingleUserNotFound.java
+++ src/test/java/tests/SingleUserNotFound.java
@@ -1,4 +1,24 @@
 package tests;
 
-public class SingleUserNotFound {
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.when;
+import static org.testng.Assert.assertEquals;
+
+public class SingleUserNotFound extends BaseTest {
+
+    @Test
+    public void tesSingleUserNotFound() {
+
+        Response response =
+                when()
+                        .get("https://reqres.in/api/users/23")
+                        .then()
+                        .log().all()
+                        .statusCode(404)
+                        .contentType(ContentType.JSON).extract().response();
+
+    }
 }
